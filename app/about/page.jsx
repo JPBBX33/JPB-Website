7ecea36081690b1d7f67cbf06ca867dfc0213fa8
Romain Hernandez <romsher.dev@gmail.com>
2024-10-21 10:07:11 +0200

    fix: Button doesn't click in philosophy box

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,10 @@
 import AboutPage from "../src/components/main/rendering/aboutPage/Index";
+
+export const metadata = {
+  title: "Jean-Pierre Bost - En savoir plus",
+  description:
+    "Découvrez mon parcours, mes compétences et mes valeurs. En tant que photographe professionnel, je mets mon savoir-faire à votre service."
+}
 export default function About() {
   return <AboutPage />;
 }
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,11 @@
 import ContactPage from "../src/components/main/rendering/contactPage/Index";
+
+export const metadata = {
+  title: "Jean-Pierre Bost - Contact",
+  description:
+    "Vous souhaitez me contacter pour une demande de devis, une question ou une collaboration ? N'hésitez pas à me laisser un message, je vous répondrai dans les plus brefs délais.",
+}
+
 export default function Contact() {
   return <ContactPage />;
 }
